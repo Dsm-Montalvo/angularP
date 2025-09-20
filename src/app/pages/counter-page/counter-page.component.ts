be973ceb28be8ae510a6adf6603ea9bde0bf118b
Dsm-Montalvo <al222210568@gmail.com>
2025-09-19 23:59:59 -0600

    Angular Practica

---
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-page',
+  imports: [],
+  templateUrl: './counter-page.component.html',
+})
+export class CounterPageComponent { }
